63d26f030ea408142d83fa899d7277c7a0fbbf74
LaooooB <billywenttocis@gmail.com>
2026-08-10 03:06:15 +0800

    Phase F: implement GestureEntered and re-arm semantics

---
+++ Source/RightGestureRuntime.cpp
@@ -0,0 +1,96 @@
+#include "RightGestureRuntime.h"
+
+namespace gr
+{
+void RightGestureRuntime::reset() noexcept
+{
+    armed = true;
+    currentGesture = ControlGesture::unknown;
+    previousGesture = ControlGesture::unknown;
+    blockedGesture = ControlGesture::unknown;
+}
+
+void RightGestureRuntime::disarmForSlotChange() noexcept
+{
+    if (currentGesture == ControlGesture::unknown)
+    {
+        armed = true;
+        blockedGesture = ControlGesture::unknown;
+        previousGesture = ControlGesture::unknown;
+        return;
+    }
+
+    armed = false;
+    blockedGesture = currentGesture;
+    previousGesture = currentGesture;
+}
+
+RightGestureRuntimeFrame RightGestureRuntime::update (bool handPresent,
+                                                      ControlGesture stableGesture) noexcept
+{
+    RightGestureRuntimeFrame frame;
+
+    if (! handPresent)
+    {
+        reset();
+        frame.armed = true;
+        return frame;
+    }
+
+    currentGesture = stableGesture;
+
+    if (! armed)
+    {
+        if (stableGesture == ControlGesture::unknown)
+        {
+            armed = true;
+            blockedGesture = ControlGesture::unknown;
+            previousGesture = ControlGesture::unknown;
+            frame.armed = true;
+            return frame;
+        }
+
+        if (stableGesture == blockedGesture)
+        {
+            frame.gesture = stableGesture;
+            frame.armed = false;
+            return frame;
+        }
+
+        armed = true;
+        blockedGesture = ControlGesture::unknown;
+        previousGesture = stableGesture;
+        frame.gesture = stableGesture;
+        frame.entered = true;
+        frame.continuousActive = true;
+        frame.armed = true;
+        return frame;
+    }
+
+    frame.armed = true;
+    frame.gesture = stableGesture;
+
+    if (stableGesture == ControlGesture::unknown)
+    {
+        previousGesture = ControlGesture::unknown;
+        return frame;
+    }
+
+    frame.continuousActive = true;
+    if (stableGesture != previousGesture)
+    {
+        frame.entered = true;
+        previousGesture = stableGesture;
+    }
+
+    return frame;
+}
+
+void RightGestureRuntime::restoreArmingState (bool shouldBeArmed, ControlGesture blocked) noexcept
+{
+    armed = shouldBeArmed;
+    blockedGesture = shouldBeArmed ? ControlGesture::unknown : blocked;
+    currentGesture = ControlGesture::unknown;
+    previousGesture = shouldBeArmed ? ControlGesture::unknown : blocked;
+}
+}
