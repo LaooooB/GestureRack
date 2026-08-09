20a3af6ab0081bc588ff38e4d1c169116f4f178f
LaooooB <billywenttocis@gmail.com>
2026-08-09 20:50:49 +0800

    Add bypass wrapper interface

---
+++ Source/GestureBypassWrapper.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <JuceHeader.h>
+#include <atomic>
+
+namespace gr
+{
+class GestureBypassWrapper final : public juce::AudioProcessor
+{
+public:
+    static constexpr int maxCompensatedLatencySamples = 524288;
+
+    GestureBypassWrapper (std::unique_ptr<juce::AudioPluginInstance> childToOwn,
+                          const juce::AudioChannelSet& inputSet,
+                          const juce::AudioChannelSet& outputSet,
+                          std::atomic<bool>& requestedBypassState);
+    ~GestureBypassWrapper() override;
+
+    juce::AudioPluginInstance* getChild() const noexcept { return child.get(); }
+    int getChildLatencySamples() const noexcept;
+
+    const juce::String getName() const override { return "Gesture Bypass Wrapper"; }
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    double getTailLengthSeconds() const override;
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override { return false; }
+    bool hasEditor() const override { return false; }
+    juce::AudioProcessorEditor* createEditor() override { return nullptr; }
+    int getNumPrograms() override { return 1; }
+    int getCurrentProgram() override { return 0; }
+    void setCurrentProgram (int) override {}
+    const juce::String getProgramName (int) override { return {}; }
+    void changeProgramName (int, const juce::String&) override {}
+    void getStateInformation (juce::MemoryBlock&) override {}
+    void setStateInformation (const void*, int) override {}
+
+private:
+    std::unique_ptr<juce::AudioPluginInstance> child;
+    std::atomic<bool>& requestedBypass;
+
+    juce::AudioBuffer<float> dryBuffer;
+    juce::dsp::DelayLine<float, juce::dsp::DelayLineInterpolationTypes::Linear> dryDelay
+        { maxCompensatedLatencySamples };
+    juce::SmoothedValue<float, juce::ValueSmoothingTypes::Linear> wetMix { 1.0f };
+    bool lastRequestedBypass = false;
+};
+}
